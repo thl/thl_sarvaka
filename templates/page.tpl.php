3fded070fd2653113d9ba40f83d630fed80c7546
ThanGrove <than@virginia.edu>
2016-05-17 17:02:46 -0400

    fixing call the shanti sarvaka breadcrumbs

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -58,7 +58,7 @@
             	}
               ?></span></h1>
               <nav class="breadwrap" role="navigation">
-                <?php print shanti_sarvaka_get_breadcrumbs($variables); ?>
+                <?php print thl_sarvaka_breadcrumb($variables); ?>
               </nav>
               <div class="banner-content">
                 <?php print render($page['banner']); ?>
