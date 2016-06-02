f2c6992b542b6690ba0d2cd53b941f75d4356c9e
ThanGrove <than@virginia.edu>
2016-06-02 09:00:29 -0400

    Adding all logos from Mark

--- js/thl-main.js
+++ js/thl-main.js
@@ -4,27 +4,13 @@
       attach: function (context, settings) {
           if (context == document) {
               $('#tree').on('useractivate', function () { 
-
+                    // When Fancy tree is activated, add "places-main" id to content div and adjust body classes
                     var cntdiv = $('#block-system-main .content div');
                     if (cntdiv.attr("id") != "places-main" || cntdiv.attr("id") != "subjects-main") {
                         cntdiv.attr("id", "places-main");
                         $('body').addClass('no-sidebars page-places page-places- page-places-317 page-places-overview page-places-overview-nojs');
-
-  
                     }
-                
                 });
-               /*console.log("fn titles: " + $(' #tree li').length);
-                window.inttime = 0;
-                window.myint = setInterval(function () {
-                    console.log(window.inttime + " : " + $('#tree').fancytree('getTree'));
-                    window.inttime += 100;
-                    if (window.inttime > 60000) {
-                        clearInterval(window.myint);
-                    }
-                }, 100);
-                $(' #tree li').click(function() {console.log("clicked");});
-                */
           }
       }
     };
